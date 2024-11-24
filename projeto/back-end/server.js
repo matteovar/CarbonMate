bcf730dc00d9f9fa69e30ad5c03356148eefbe30
matteovar <matvarnier@gmail.com>
2024-11-23 21:41:45 -0300

    dada

--- projeto/back-end/server.js
+++ projeto/back-end/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -12,22 +13,23 @@ const PORT = 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Servir arquivos estáticos da pasta 'view'
-app.use(express.static('view'));
+// Serve os arquivos estáticos da pasta 'view', que está fora da pasta 'back-end'
+// Usando path.join para garantir que o caminho seja resolvido corretamente
+app.use(express.static(path.join(__dirname, '..', 'view')));
 
 // Conexão com o MongoDB Atlas
-const uri = 'mongodb+srv://Matteo03:Matteo03@cluster0.qmljj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+const uri = process.env.MONGO_URL || 'mongodb+srv://Matteo03:Matteo03@cluster0.qmljj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+console.log('String de conexão:', uri); // Log para verificar a string de conexão
+
+mongoose.connect(uri)
 .then(() => console.log('Conectado ao MongoDB Atlas'))
 .catch(err => console.error('Erro ao conectar ao MongoDB Atlas:', err));
 
 app.use('/api/users', userRoutes);
 
+// Rota inicial (opcional)
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/view/index.html');
+  res.sendFile(path.join(__dirname, '..', 'view', 'index.html'));  // Ajuste o caminho aqui também
 });
 
 // Iniciar o servidor
